d9d6582f0fca320ff7fbb3b2847710f53df62942
Shanol <102300466+shanolhere@users.noreply.github.com>
2022-06-18 13:40:07 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -22,3 +22,23 @@ const generateJoke = () => {
 };
 
 btn.addEventListener("click", generateJoke);
+
+// here I have used promises and fetch..
+// we can use async await too. Using async and await makes life easy.
+
+// const generateJoke = async () => {
+// try{
+//   const setHeader = {
+//   headers: {
+//     Accept: "application/JSON"
+//   }
+// };
+
+// const res = await fetch("https://icanhazdadjoke.com/", setHeader);
+// const data = await res.json();
+// joke.innerHTML = data.joke;
+// }catch(err){
+//   console.log(`The error is: $(err)`);
+// }
+
+// }
